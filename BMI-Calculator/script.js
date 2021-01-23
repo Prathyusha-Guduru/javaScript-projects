1cba6e2128bfc1f9117f5e309d0b0d5a85413bb6
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-01-23 19:21:46 +0530

    Updated script.js

--- BMI-Calculator/script.js
+++ BMI-Calculator/script.js
@@ -1,9 +1,17 @@
+document.getElementById()
+
+
+
+
+
+
+
 document.getElementById('form').addEventListener('submit',(event)=>{
 	event.preventDefault();
 	let weight = document.getElementById('weight').value;
 	let height = document.getElementById('height').value;
-	// console.log(`weight is ${weight},height is ${height}`);
 	let bmi = (weight/(height*height)).toFixed(2);
 	let result = document.getElementById('result');
-	result.innerHTML = `<p>Your Body Mass Index is <strong>${bmi}</strong></p>`
+	result.innerHTML = `<p>Your Body Mass Index is <strong>${bmi}</strong></p>`;
+
 })
