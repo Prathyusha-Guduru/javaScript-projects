3b9d6df80dc77154764b67507fb9581b1812a3f1
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-01-24 10:45:57 +0530

    Updated responsive css

--- Dice-Game/script.js
+++ Dice-Game/script.js
@@ -23,3 +23,20 @@ playButton.addEventListener('click',(event)=>{
 	}
 	console.log(`randomNumber1 : ${randomNumber1} and randomNumber2 : ${randomNumber2}`);
 })
+
+
+window.onload = ()=>{
+	let randomNumber1 = Math.floor(Math.random() * diceImages.length)
+	let randomNumber2 = Math.floor(Math.random()*diceImages.length)
+	player1Dice.innerHTML = `<img src=${diceImages[randomNumber1]} alt="" srcset="" id="dice-img">`
+	player2Dice.innerHTML = `<img src=${diceImages[randomNumber2]} alt="" srcset="" id="dice-img">`
+	randomNumber1>randomNumber2 ? winner = "Player 1" : winner = "Player 2"
+	randomNumber1 === randomNumber2 ? draw = true : draw = false
+	if(draw === false){
+		result.innerHTML = `<h2>${winner} wins</h2>`
+	}
+	else if(draw === true){
+		result.innerHTML = `<h2>It is a draw!</h2>`
+	}
+	console.log(`randomNumber1 : ${randomNumber1} and randomNumber2 : ${randomNumber2}`);
+}
