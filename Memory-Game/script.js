c716f0ecaf83a0a71073e0d942b4b1a3e99feeb0
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-01-25 11:22:56 +0530

    Updated cards

---
+++ Memory-Game/script.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded',()=>{
+	const cardArray = [
+		{
+			name : 'card1',
+			img  : 'assets/images/memory-card-1.png'
+		},
+		{
+			name : 'card1',
+			img  : 'assets/images/memory-card-1.png'
+		},
+		{
+			name : 'card2',
+			img  : 'assets/images/memory-card-2.png'
+		},
+		{
+			name : 'card2',
+			img  : 'assets/images/memory-card-2.png'
+		},
+		{
+			name : 'card3',
+			img  : 'assets/images/memory-card-3.png'
+		},
+		{
+			name : 'card3',
+			img  : 'assets/images/memory-card-3.png'
+		},
+		{
+			name : 'card4',
+			img  : 'assets/images/memory-card-4.png'
+		},
+		{
+			name : 'card4',
+			img  : 'assets/images/memory-card-4.png'
+		},
+		{
+			name : 'card5',
+			img  : 'assets/images/memory-card-5.png'
+		},
+		{
+			name : 'card5',
+			img  : 'assets/images/memory-card-5.png'
+		},
+		{
+			name : 'card6',
+			img  : 'assets/images/memory-card-6.png'
+		},
+		{
+			name : 'card6',
+			img  : 'assets/images/memory-card-6.png'
+		}
+	]
+})
