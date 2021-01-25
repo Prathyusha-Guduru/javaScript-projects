79cd474c7ba3e90615e4b92c2b5a3318732ccd84
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-01-25 13:04:15 +0530

    Updated script.js

--- Memory-Game/script.js
+++ Memory-Game/script.js
@@ -1,5 +1,3 @@
-
-
 const cardArray = [
 	{
 		name : 'card1',
@@ -51,16 +49,14 @@ const cardArray = [
 	}
 ]
 
+const grid = document.getElementsByClassName('grid')[0]
+let cardsChosenPair = []
+let cardsChosenPairId = []
 
 
-const grid = document.querySelector('.grid')
-let cardsChosen = []
-let cardsChosenId = []
-let cardsWon = []
-let result = document.getElementById('result')
+//Creating the game board
 
-
-function createBoard(){
+function createGameBoard(){
 	for(let i=0;i<cardArray.length;i++){
 		var card = document.createElement('img')
 		card.setAttribute('src','assets/images/cover-img.png')
@@ -70,38 +66,67 @@ function createBoard(){
 	}
 }
 
-//Check for matches
-function checkForMatch(){
-	var cards = document.querySelectorAll('img')
-	const optionOneId = cardsChosenId[0]
-	const optionTwoId = cardsChosenId[1]
-	if (cardsChosen[0] === cardsChosen[1]){
-		alert("You found a match")
-		cards[optionOneId].setAttribute('src','assets/images/blank-img.png')
-		cards[optionTwoId].setAttribute('src','assets/images/blank-img.png')
-		cardsWon.push(cardsChosen)
-	}
-	else{
-		cards[optionOneId].setAttribute('src','assets/images/cover-img.png')
-		cards[optionTwoId].setAttribute('src','assets/images/cover-img.png')
-	}
-	cardsChosen = []
-	cardsChosenId = []
-
-}
-//Flip the card
-function flipCard()
-{
-	var cardId = this.getAttribute('id')
-	cardsChosen.push(cardArray[cardId].name)
-	cardsChosenId.push(cardId)
-	this.setAttribute('src',cardArray[cardId].img)
-	if(cardsChosen.length === 2){
-		setTimeout(checkForMatch,500)
-	}
+function flipCard(){
+	let cardId = this.getAttribute('id')
+	console.log(cardId);
 }
 
-createBoard()
+
+
+
+createGameBoard()
+
+
+// const grid = document.querySelector('.grid')
+// let cardsChosen = []
+// let cardsChosenId = []
+// let cardsWon = []
+// let result = document.getElementById('result')
+
+
+// function createBoard(){
+// 	for(let i=0;i<cardArray.length;i++){
+// 		var card = document.createElement('img')
+// 		card.setAttribute('src','assets/images/cover-img.png')
+// 		card.setAttribute('id',i)
+// 		card.addEventListener('click',flipCard)
+// 		grid.appendChild(card)
+// 	}
+// }
+
+// //Check for matches
+// function checkForMatch(){
+// 	var cards = document.querySelectorAll('img')
+// 	const optionOneId = cardsChosenId[0]
+// 	const optionTwoId = cardsChosenId[1]
+// 	if (cardsChosen[0] === cardsChosen[1]){
+// 		alert("You found a match")
+// 		cards[optionOneId].setAttribute('src','assets/images/blank-img.png')
+// 		cards[optionTwoId].setAttribute('src','assets/images/blank-img.png')
+// 		cardsWon.push(cardsChosen)
+// 	}
+// 	else{
+// 		cards[optionOneId].setAttribute('src','assets/images/cover-img.png')
+// 		cards[optionTwoId].setAttribute('src','assets/images/cover-img.png')
+// 	}
+// 	cardsChosen = []
+// 	cardsChosenId = []
+
+// }
+// //Flip the card
+// function flipCard()
+// {
+
+// 	var cardId = this.getAttribute('id')
+// 	cardsChosen.push(cardArray[cardId].name)
+// 	cardsChosenId.push(cardId)
+// 	this.setAttribute('src',cardArray[cardId].img)
+// 	if(cardsChosen.length === 2){
+// 		setTimeout(checkForMatch,500)
+// 	}
+// }
+
+// createBoard()
 
 
 
