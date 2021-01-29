8da49e01c71a889a710c3097c0f55a9407c35e4e
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-01-29 22:56:00 +0530

    Updated script.js

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -3,7 +3,21 @@ let listDisplay = document.getElementById('list-item-display')
 form.addEventListener('submit',(event)=>{
 	event.preventDefault()
 	let newItem = document.getElementById('new-list-item')
-	let newItemDisplay = document.createElement('p')
-	newItemDisplay.innerHTML = newItem.value
-	listDisplay.appendChild(new)
+	let newItemDisplay = document.createElement('div')
+	newItemDisplay.classList.add('list-items')
+	newItemDisplay.innerHTML = `<p class = "new-item-value">${newItem.value}</p> <input type="checkbox" class = "isChecked" name = "checkbox">`
+	listDisplay.appendChild(newItemDisplay)
+})
+
+
+let checkBox = document.querySelectorAll("input[name = 'checkbox']")	
+checkBox.forEach((element)=>{
+	element.addEventListener('click',()=>{
+		if(element.checked === true){
+			document.querySelector('.new-item-value').style.textDecoration = "line-through"
+		}
+		else{
+			document.querySelector('.new-item-value').style.textDecoration = "initial"
+		}
+	})
 })
