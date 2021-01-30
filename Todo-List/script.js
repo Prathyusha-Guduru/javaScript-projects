b643abc4f8b1fe71eb53cce745bf6ebfabed72cf
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-01-30 13:22:52 +0530

    Updated list entries

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -1,23 +1,38 @@
-let form = document.getElementById('form')
-let listDisplay = document.getElementById('list-item-display')
-form.addEventListener('submit',(event)=>{
+let listItemDisplay = document.getElementById('list-item-display')
+let newItemInputForm = document.getElementById('form')
+let list = []
+let n = 0
+newItemInputForm.addEventListener('submit',(event)=>{
 	event.preventDefault()
-	let newItem = document.getElementById('new-list-item')
-	let newItemDisplay = document.createElement('div')
-	newItemDisplay.classList.add('list-items')
-	newItemDisplay.innerHTML = `<p class = "new-item-value">${newItem.value}</p> <input type="checkbox" class = "isChecked" name = "checkbox">`
-	listDisplay.appendChild(newItemDisplay)
-})
-
-
-let checkBox = document.querySelectorAll("input[name = 'checkbox']")	
-checkBox.forEach((element)=>{
-	element.addEventListener('click',()=>{
-		if(element.checked === true){
-			document.querySelector('.new-item-value').style.textDecoration = "line-through"
-		}
-		else{
-			document.querySelector('.new-item-value').style.textDecoration = "initial"
-		}
+	n+=1
+	let newListEntry = document.getElementById('new-list-item-entry').value
+	let newListDiv = document.createElement('div')
+	newListDiv.setAttribute("id",`${n}`)
+	newListDiv.classList.add('list-items')
+	newListDiv.innerHTML = `<p class = "${n}p">${newListEntry}</p> <input class = "${n}checked" type = "checkbox" name = "checkbox">`
+	listItemDisplay.appendChild(newListDiv)
+	let checkBoxes = document.querySelectorAll('input[type = "checkbox"]')
+	checkBoxes.forEach((element)=>
+	{
+		element.addEventListener('click',()=>{
+			if(element.checked === true){
+				let elementClass = element.className
+				let elementContent = document.getElementsByClassName(elementClass[0]+'p')[0]
+				elementContent.style.textDecoration = "line-through"
+			}
+			else if(element.checked === false){
+				let elementClass = element.className
+				let elementContent = document.getElementsByClassName(elementClass[0]+'p')[0]
+				elementContent.style.textDecoration = "initial"
+			}
+		})
 	})
+
 })
+
+
+
+
+
+
+
