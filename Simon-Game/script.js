987c54f49bfa3da4f4495a0919e883280ece9563
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-06-27 21:54:16 +0530

    Updated sound and animation on button clicks

--- Simon-Game/script.js
+++ Simon-Game/script.js
@@ -1,10 +1,32 @@
 boxes = ['box-0','box-1','box-2','box-3']
+sounds = ['sound-1','sound-2','sound-3','sound-0']
+gamePattern = []
+userClickedPattern = []
 
 function nextSequence(){
 	randomNumber =  Math.floor(Math.random() * (4 - 0) + 0);
-	gamePattern = []
 	randomChoosenBox = boxes[randomNumber]
 	gamePattern.push(randomChoosenBox)
 }
 
-console.log(gamePattern);
+function playSound(boxNum){
+	let music = document.createElement("audio")
+	music.src = "/Simon-Game/sounds/" + sounds[boxNum] + ".mp3"
+	music.play()
+}
+
+
+$('.box').click(function(){
+	boxId = $(this).attr('id')
+	boxNum = boxId.slice(-1)
+	playSound(boxNum)
+	$(this).fadeOut(100).fadeIn(100)
+	userClickedPattern.push(boxNum)
+	console.log(userClickedPattern);
+})
+
+
+
+
+
+
